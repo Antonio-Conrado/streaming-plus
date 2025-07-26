1ad7120326afd005a88ec3046778047d6fdb9532
Antonio-Conrado <jconradolopez19@gmail.com>
2025-07-25 19:43:35 -0600

    added new const

--- src/shared/data/const.ts
+++ src/shared/data/const.ts
@@ -2,3 +2,4 @@ export const TOKEN = process.env.NEXT_PUBLIC_TOKEN;
 export const imageLink = process.env.NEXT_PUBLIC_IMAGE_LINK;
 export const trailerLink = process.env.NEXT_PUBLIC_TRAILER_URL;
 export const playVideo = process.env.NEXT_PUBLIC_PLAY_VIDEO;
+export const playSerie = process.env.NEXT_PUBLIC_PLAY_SERIE;
